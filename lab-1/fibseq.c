fe8e59f715347f83c04c6934deac334fd8073740
Nienzu <ibqqz0602@gmail.com>
2015-09-27 15:43:56 +0800

    fibseq: Add time_spent record code.

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-
+#include <time.h>
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
-
+  FILE *fp;
+  fp=fopen("rec_time.txt","a");
+  clock_t start, end;
+  double time_spent;
   scanf("%d",&number);
+  start=clock();
   result = fibonacci(number);   
+  end=clock();
+  time_spent=(double)(end-start)/CLOCKS_PER_SEC;
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+  fprintf(fp, "%f\r\n", time_spent);
+  fclose(fp);
 }
 

bd74f7511143e5ad15a1d2cdf4b9b45238b0fc42
Nienzu <ibqqz0602@gmail.com>
2015-09-27 15:48:25 +0800

    fib.s: Finish the iteration of fibonacci series.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -9,35 +9,19 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+	mov r1, #0
+	mov r2, #1
+.L3:
+	subs r0, r0, #1
+	add r3, r1, r2
+	mov r1, r2
+	mov r2, r3
+	bgt .L3
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	mov r0, r1
+	bx lr
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
